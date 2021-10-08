af08bf435b53b6a568db224ae186e3c2c887f97e
Alisson Zampietro <alisson.zampietro@gmail.com>
2021-10-08 01:35:44 +0100

    feat: add methods to set store values

--- public/js/store.js
+++ public/js/store.js
@@ -6,3 +6,46 @@ let state = {
     allowConnectionsFromStrangers: false,
     screenSharingActive: false
 }
+
+export const setSocketId = (socketId) => {
+    state = {
+        ...state,
+        socketId: socketId
+    }
+    console.log(state);
+}
+
+export const setLocalStream = (localStream) => {
+    state = {
+        ...state,
+        localStream: localStream
+    }
+}
+
+export const allowConnectionsFromStrangers = (allowConnectionsFromStrangers) => {
+    state = {
+        ...state,
+        allowConnectionsFromStrangers: allowConnectionsFromStrangers
+    }
+}
+
+export const setScreenSharingActive = (screenSharingActive) => {
+    state = {
+        ...state,
+        screenSharingActive: screenSharingActive
+    }
+}
+
+export const setScreenSharingStream = (screenSharingStream) => {
+    state = {
+        ...state,
+        screenSharingStream: screenSharingStream
+    }
+}
+
+export const setRemoteStream = (remoteStream) => {
+    state = {
+        ...state,
+        remoteStream: remoteStream
+    }
+}
