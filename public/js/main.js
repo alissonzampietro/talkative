e1626696ab3804d74957f9566c1bfd8adbaebbc5
Alisson Zampietro <alisson.zampietro@gmail.com>
2021-10-08 01:35:23 +0100

    feat: create store file to handle global object

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+import * as store from './store.js';
+
 const socket = io('/');
 
 socket.on('connect', () => {
-    console.log('succesfully connected to web server');
-    console.log(socket.id)
+    console.log('succesfully connected to web server: ',socket.id);
+    store.setSocketId(socket.id);
 })
---
+++ public/js/store.js
@@ -0,0 +1,8 @@
+let state = {
+    socketId: null,
+    localStream: null,
+    remoteStream: null,
+    screenSharingStream: null,
+    allowConnectionsFromStrangers: false,
+    screenSharingActive: false
+}
